ecf1d296a8d139f87387293e09cad9ab6c77f90c
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-24 02:13:09 +0100

    more test case

--- Sources/WebComponents/article/article-item-test.swift
+++ Sources/WebComponents/article/article-item-test.swift
@@ -191,6 +191,14 @@ public enum _ArticleTest {
                         comment: "Second mention: contrast with operant formulation."
                     )
                 },
+
+                HTML.p {
+                    HTML.text("Hier nog is een citation. ")
+                    Citation(
+                        _TestRefs.beyond_cortisol,
+                        // comment: "Second mention: contrast with operant formulation."
+                    )
+                },
             ]
         },
     )
@@ -198,17 +206,41 @@ public enum _ArticleTest {
 
 public enum _TestRefs: String, Referencable {
     case some_ref_example
+    case beyond_cortisol
 
-    public var title: String {
-        return "Some ref example"
+    private struct Data {
+        let title: String
+        let url: String
+        let authorLine: String?
+        let dateISO8601: String?
+        let doi: String?
     }
 
-    public var url: String {
-        return "https://example.com"
+    private var data: Data {
+        switch self {
+        case .some_ref_example:
+            return Data(
+                title: "Some ref example",
+                url: "https://example.com",
+                authorLine: "John Doe et al.",
+                dateISO8601: "2025-10-25",
+                doi: "DOI.10/213023"
+            )
+
+        case .beyond_cortisol:
+            return Data(
+                title: "Beyond Cortisol! Physiological Indicators of Welfare for Dogs: Deficits, Misunderstandings and Opportunities",
+                url: "https://arxiv.org/abs/2502.11384",
+                authorLine: "ML Cobb, AG Jimenez, NA Dreschel",
+                dateISO8601: "2025-02-17",
+                doi: "10.48550/arXiv.2502.11384"
+            )
+        }
     }
 
-    public var authorLine: String? { "John Doe et al." }
-    public var dateISO8601: String? { "2025-10-25" }
-    public var doi: String? { "DOI.10/213023" }
+    public var title: String { data.title }
+    public var url: String { data.url }
+    public var authorLine: String? { data.authorLine }
+    public var dateISO8601: String? { data.dateISO8601 }
+    public var doi: String? { data.doi }
 }
-
