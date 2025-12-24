3e6ee878c48181c453e95f2c6fe8f68ee5c01c96
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-24 02:24:53 +0100

    render refs without span

--- Sources/WebComponents/article/reference-node.swift
+++ Sources/WebComponents/article/reference-node.swift
@@ -15,11 +15,47 @@ public struct Reference: HTMLNode {
     public func render(options: HTMLRenderOptions, indent: Int) -> String {
         let ref = reference
 
-        var children: [any HTMLNode] = []
+        var meta: [any HTMLNode] = []
 
-        children.append(
+        if let author = ref.authorLine, !author.isEmpty {
+            meta.append(
+                HTMLElement(
+                    "div",
+                    attrs: ["class": "ref-author"],
+                    children: [
+                        HTMLText(author)
+                    ]
+                )
+            )
+        }
+
+        if let date = ref.dateISO8601, !date.isEmpty {
+            meta.append(
+                HTMLElement(
+                    "div",
+                    attrs: ["class": "ref-date"],
+                    children: [
+                        HTMLText(date)
+                    ]
+                )
+            )
+        }
+
+        if let doi = ref.doi, !doi.isEmpty {
+            meta.append(
+                HTMLElement(
+                    "div",
+                    attrs: ["class": "ref-doi"],
+                    children: [
+                        HTMLText("DOI: \(doi)")
+                    ]
+                )
+            )
+        }
+
+        var children: [any HTMLNode] = [
             HTMLElement(
-                "span",
+                "div",
                 attrs: ["class": "ref-title"],
                 children: [
                     HTMLElement(
@@ -31,40 +67,14 @@ public struct Reference: HTMLNode {
                     )
                 ]
             )
-        )
+        ]
 
-        if let author = ref.authorLine, !author.isEmpty {
+        if !meta.isEmpty {
             children.append(
                 HTMLElement(
-                    "span",
-                    attrs: ["class": "ref-author"],
-                    children: [
-                        HTMLText(" — \(author)")
-                    ]
-                )
-            )
-        }
-
-        if let date = ref.dateISO8601, !date.isEmpty {
-            children.append(
-                HTMLElement(
-                    "span",
-                    attrs: ["class": "ref-date"],
-                    children: [
-                        HTMLText(" (\(date))")
-                    ]
-                )
-            )
-        }
-
-        if let doi = ref.doi, !doi.isEmpty {
-            children.append(
-                HTMLElement(
-                    "span",
-                    attrs: ["class": "ref-doi"],
-                    children: [
-                        HTMLText(" DOI: \(doi)")
-                    ]
+                    "div",
+                    attrs: ["class": "ref-meta"],
+                    children: meta
                 )
             )
         }
@@ -90,4 +100,83 @@ public struct Reference: HTMLNode {
             children: children
         ).render(options: options, indent: indent)
     }
+
+    // public func render(options: HTMLRenderOptions, indent: Int) -> String {
+    //     let ref = reference
+
+    //     var children: [any HTMLNode] = []
+
+    //     children.append(
+    //         HTMLElement(
+    //             "span",
+    //             attrs: ["class": "ref-title"],
+    //             children: [
+    //                 HTMLElement(
+    //                     "a",
+    //                     attrs: ["href": ref.url],
+    //                     children: [
+    //                         HTMLText(ref.title)
+    //                     ]
+    //                 )
+    //             ]
+    //         )
+    //     )
+
+    //     if let author = ref.authorLine, !author.isEmpty {
+    //         children.append(
+    //             HTMLElement(
+    //                 "span",
+    //                 attrs: ["class": "ref-author"],
+    //                 children: [
+    //                     HTMLText(" — \(author)")
+    //                 ]
+    //             )
+    //         )
+    //     }
+
+    //     if let date = ref.dateISO8601, !date.isEmpty {
+    //         children.append(
+    //             HTMLElement(
+    //                 "span",
+    //                 attrs: ["class": "ref-date"],
+    //                 children: [
+    //                     HTMLText(" (\(date))")
+    //                 ]
+    //             )
+    //         )
+    //     }
+
+    //     if let doi = ref.doi, !doi.isEmpty {
+    //         children.append(
+    //             HTMLElement(
+    //                 "span",
+    //                 attrs: ["class": "ref-doi"],
+    //                 children: [
+    //                     HTMLText(" DOI: \(doi)")
+    //                 ]
+    //             )
+    //         )
+    //     }
+
+    //     for c in comments where !c.isEmpty {
+    //         children.append(
+    //             HTMLElement(
+    //                 "div",
+    //                 attrs: ["class": "ref-comment"],
+    //                 children: [
+    //                     HTMLText(c)
+    //                 ]
+    //             )
+    //         )
+    //     }
+
+    //     return HTMLElement(
+    //         "li",
+    //         attrs: [
+    //             "class": "ref-item",
+    //             "id": "ref-\(ref.public_name_or_id)"
+    //         ],
+    //         children: children
+    //     ).render(options: options, indent: indent)
+    // }
 }
