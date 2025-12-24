4128835ecf3113b2510eebbef62da74a50b15bee
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-25 00:20:20 +0100

    deprecate articletarget

--- Sources/WebComponents/article/article/article-target.swift
+++ Sources/WebComponents/article/article/article-target.swift
@@ -1,38 +1,38 @@
-import Constructors
-import HTML
-import Path
-import Primitives
+// import Constructors
+// import HTML
+// import Path
+// import Primitives
 
-public struct ArticleTarget: Targetable, MetadataTargetable {
-    public let article_item: ArticleItem
+// public struct ArticleTarget: Targetable, MetadataTargetable {
+//     public let article_item: ArticleItem
 
-    // Derived render function
-    public let html: @Sendable () -> HTMLDocument
+//     // Derived render function
+//     public let html: @Sendable () -> HTMLDocument
 
-    public let output: StandardPath
-    public let visibility: Set<BuildEnvironment>
-    public let navigation: NavigationSetting
+//     public let output: StandardPath
+//     public let visibility: Set<BuildEnvironment>
+//     public let navigation: NavigationSetting
 
-    public let metadata: TargetMetadata
+//     public let metadata: TargetMetadata
 
-    public init(
-        article_item: ArticleItem,
-        article_html_provider: @Sendable @escaping (ArticleItem) -> HTMLDocument,
-        output: StandardPath,
-        visibility: Set<BuildEnvironment> = [.local, .test, .public],
-        navigation: NavigationSetting = .none,
-        metadata: TargetMetadata? = nil
-    ) {
-        self.article_item = article_item
-        self.html = { article_html_provider(article_item) }
-        self.output = output
-        self.visibility = visibility
-        self.navigation = navigation
-        self.metadata =
-            metadata.exists_or_inits(visibility: visibility)
-    }
+//     public init(
+//         article_item: ArticleItem,
+//         article_html_provider: @Sendable @escaping (ArticleItem) -> HTMLDocument,
+//         output: StandardPath,
+//         visibility: Set<BuildEnvironment> = [.local, .test, .public],
+//         navigation: NavigationSetting = .none,
+//         metadata: TargetMetadata? = nil
+//     ) {
+//         self.article_item = article_item
+//         self.html = { article_html_provider(article_item) }
+//         self.output = output
+//         self.visibility = visibility
+//         self.navigation = navigation
+//         self.metadata =
+//             metadata.exists_or_inits(visibility: visibility)
+//     }
 
-    public func document() -> HTMLDocument {
-        html()
-    }
-}
+//     public func document() -> HTMLDocument {
+//         html()
+//     }
+// }
