ef7dabf13e50393012677a58ac3325f7da0e7fda
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-29 15:21:47 +0100

    rm comment

--- Sources/WebComponents/article/article/article-item-element.swift
+++ Sources/WebComponents/article/article/article-item-element.swift
@@ -33,33 +33,6 @@ public enum ArticleItemElement {
             }
         }
 
-        // public static func html(
-        //     href: String,
-        //     label: HTMLFragment,
-        //     item: ArticleItem
-        // ) -> any HTMLNode {
-        //     HTML.span(["class": "hoverref"]) {
-        //         HTML.a(href, ["class": "hoverref__link"]) {
-        //             label
-        //         }
-
-        //         HTML.span(["class": "hoverref__card"]) {
-        //             if let thumb = item.thumbnail_src {
-        //                 HTML.img(
-        //                     src: thumb.rendered(asRootPath: true),
-        //                     alt: item.title,
-        //                     ["class": "hoverref__thumb"]
-        //                 )
-        //             }
-
-        //             HTML.span(["class": "hoverref__meta"]) {
-        //                 HTML.span(["class": "hoverref__title"]) { HTML.text(item.title) }
-        //                 HTML.span(["class": "hoverref__def"]) { item.definition() }
-        //             }
-        //         }
-        //     }
-        // }
-
         public static func css(z_index: Int = 1000) -> CSSStyleSheet {
             CSSStyleSheet(
                 rules: [
