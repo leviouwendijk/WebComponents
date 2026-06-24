c84d876a575c5b9887e42209997fa7c66be35346
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-06-24 12:30:50 +0200

    apis

--- Sources/WebComponents/article/extensions/exts-html.swift
+++ Sources/WebComponents/article/extensions/exts-html.swift
@@ -2,13 +2,34 @@ import HTML
 import References
 
 public extension HTML {
-    static func cite(_ ref: any Referencable) -> any HTMLNode {
+    static func cite(
+        _ ref: any Referencable
+    ) -> any HTMLNode {
         Citation(ref)
+    }
+
+    static func cite(
+        _ ref: any Referencable,
+        comment: String?
+    ) -> any HTMLNode {
+        Citation(
+            ref,
+            comment: comment
+        )
     }
 }
 
 public extension Referencable {
     func cite() -> any HTMLNode {
         HTML.cite(self)
+    }
+
+    func cite(
+        comment: String?
+    ) -> any HTMLNode {
+        HTML.cite(
+            self,
+            comment: comment
+        )
     }
 }
